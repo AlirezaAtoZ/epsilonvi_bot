4d65d565a369478f36450d4d552083db18dee725
alireza_host <epsilonv@epsilonvi.ir>
2023-10-01 11:03:36 +0200

    feat: init on host

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,11 +1,16 @@
-"""
-WSGI config for app project.
+# import os
+# import sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
+# sys.path.insert(0, os.path.dirname(__file__))
+
+
+# def application(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'It works!  but why the website is not loading?\n'
+#     version = 'Python v' + sys.version.split()[0] + '\n'
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
 
 import os
 
