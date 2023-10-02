f848d8db51d343be074e37585411af0e29e27240
alireza_host <epsilonv@epsilonvi.ir>
2023-10-02 15:21:26 +0200

    fix: set_webhook file

--- app/bot/management/commands/set_webhook.py
+++ app/bot/management/commands/set_webhook.py
@@ -2,22 +2,25 @@ from typing import Any
 from django.core.management.base import BaseCommand, CommandError
 from app.urls import urlpatterns
 import requests
+import os
+import json
 
 
 class Command(BaseCommand):
     help = "sets the webhook url"
     # TODO: softcode it!
-    webhook_url = 'epsilonvi.ir/bot-webhook-63af1eda-28f9-4f21-b8df-fb73453f9892/'
-    secret_token = 'ceb5481d-6c93-4954-b853-cfb31cecb786'
-    set_webhook_url = 'https://api.telegram.org/bot600565689:AAFh_EUMqwILLe6sE0jp1lNTVOTQsL2pCvY/setWebhook'
-    params = {'X-Telegram-Bot-Api-Secret-Token': secret_token}
+    webhook_url = 'https://epsilonvi.ir/bot-webhook-63af1eda-28f9-4f21-b8df-fb73453f9892'
+    secret_token = os.environ.get('EPSILONVI_DEV_SECRET_TOKEN')
+    set_webhook_url = f'https://api.telegram.org/bot{os.environ.get("EPSILONVI_DEV_BOT_TOKEN")}/setWebhook'
+    params = {
+	'url': webhook_url,
+	'X-Telegram-Bot-Api-Secret-Token': secret_token}
 
-    def handle(self, *args: Any, **options: Any) -> str | None:
-        res = requests.get(
-            url=self.set_webhook_url,
-            params=self.params)
+    def handle(self, *args: Any, **options: Any):
+        res = requests.get(url=self.set_webhook_url, params=self.params)
         
+        if not res.ok:
+            raise CommandError(str(res.json()))
+
         data = res.json()
-
-        self.stdout.write(str(data))
-        
+        self.stdout.write(self.style.SUCCESS(data['description']))
