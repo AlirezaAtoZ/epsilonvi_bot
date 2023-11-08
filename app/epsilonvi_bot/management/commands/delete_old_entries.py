6baaff7aeb8ddd7229e381bc926fa2b7746a45d4
alireza_host <epsilonv@epsilonvi.ir>
2023-11-08 15:36:31 +0100

    feat: delete old secret codes

--- app/epsilonvi_bot/management/commands/delete_old_entries.py
+++ app/epsilonvi_bot/management/commands/delete_old_entries.py
@@ -1,6 +1,16 @@
-from django.core.management.base import BaseCommand, CommandError
+from datetime import timedelta
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
 from bot.models import UpdateID
+from epsilonvi_bot.models import SecretCode
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        UpdateID.objects.filter()
+        # delete old secret codes
+        limit = timezone.now() - timedelta(days=3)
+        scs = SecretCode.objects.filter(created__lt=limit)
+        scs.delete()
+
+        # TODO delete old updates
